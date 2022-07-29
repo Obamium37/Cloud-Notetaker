cb65cceca48735a14ac98842f734d17a501858f0
anakcoder123 <101948161+anakcoder123@users.noreply.github.com>
2022-07-28 20:20:13 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+// Packages Imported
+
 import { useState, useEffect } from 'react';
 import './App.css';
 import { db } from './firebase-config';
 import {collection, getDocs, addDoc, deleteDoc, doc, updateDoc} from "firebase/firestore"; 
-import { async } from '@firebase/util';
 
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
 
 
 
-
+  // To create Note
   const createNote = async () => {
     setNewNote("")
     setNewData("")
@@ -28,13 +29,14 @@ function App() {
 
 
 
-
+  // Delete Note
 
   const deleteNote = async (id) => {
     const userDoc = doc(db, "username", id);
     await deleteDoc(userDoc);
   }
 
+  // Update Note
 
   const updateNote = async (id, notes) => {
     const userDoc = doc(db, "username", id);
@@ -59,13 +61,36 @@ function App() {
   }, [usernameCollectionRef])
 
 
+  //Basic Inline Styling 
 
+  const buttonStyle = {
+    font: '25',
+    color: 'blue'
+  }
+
+  const noteStyle = {
+
+    color: 'green'
+  }
+
+  const textareaStyle = {
+    color: 'red'
+  }
+
+  const deleteStyle = {
+
+    color: 'purple'
+  }
+
+
+
+// Main display for the function
 
   return (
 
     <div className="App">
-      <input placeholder='New Note Name' onChange={(event) => {setNewNote(event.target.value)}}/>
-      <textarea type="textarea" placeholder='Enter Notes' onChange={(event) => {setNewData(event.target.value)}}/>
+      <input style={textareaStyle} placeholder='New Note Name' onChange={(event) => {setNewNote(event.target.value)}}/>
+      <textarea style={textareaStyle} type="textarea" placeholder='Enter Notes' onChange={(event) => {setNewData(event.target.value)}}/>
       <button onClick={createNote}> Create Note</button>
       {" "}
       {username.map((username) => {
@@ -73,14 +98,14 @@ function App() {
         return (
         <div>
           {" "}
-          <h1>Name: {username.name} </h1>
-          <p>Notes: {username.notes} </p>
+          <h1 className='primary'>Name: {username.name} </h1>
+          <p style={noteStyle} >Notes: {username.notes} </p>
 
-          <textarea type="textarea" value={input} placeholder='Enter Edit' onChange={(event) => {setInput(event.target.value)}}/>
-          <button onClick={() => {updateNote(username.id, username.notes)}}>Submit</button>        
-          <button onClick={() => {deleteNote(username.id)}}>Delete Note</button>
+          <textarea style={noteStyle} type="textarea" value={input} placeholder='Enter Edit' onChange={(event) => {setInput(event.target.value)}}/>
+          <button style={buttonStyle} onClick={() => {updateNote(username.id, username.notes)}}>Submit</button>        
+          <button style={deleteStyle} onClick={() => {deleteNote(username.id)}}>Delete Note</button>
           
-        </div>
+        </div>  
         ) 
       })}
     </div>
@@ -89,3 +114,4 @@ function App() {
 }
 
 export default App;
+
